262a6d5f8d3ad77c3703831194acb1b2d5bd1454
Kyru Cabading <kyrusri@gmail.com>
2018-11-16 00:39:44 +0800

    Added title and favicon

--- components/Meta.js
+++ components/Meta.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 
 export default () => (
   <Head>
+    <title>GoScope</title>
+    <link rel='shortcut icon' type="image/png" href='./static/assets/favicon.png' />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta charSet="utf-8" />
     <link rel="stylesheet" href="/static/base.css" />
